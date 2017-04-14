be2f61786c2bbb2d073e66c453d6fbe2205d4268
Alex Linkov <alexey.linkov@gmail.com>
2017-04-14 23:40:22 +0200

    add pop sorting;

--- falconrypro/app/admin/bird_type.rb
+++ falconrypro/app/admin/bird_type.rb
@@ -1,8 +1,9 @@
 ActiveAdmin.register BirdType do
-  permit_params :name, :latin, :group_id
+  permit_params :name, :latin, :group_id, :popular
   before_filter :skip_sidebar!, :only => :index
   index  do
   selectable_column
+  column :popular
   column :name
   column :latin
   column :group
