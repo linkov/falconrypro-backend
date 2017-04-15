af1552b93afd830d134ed79cb12da3860e72be4c
Alex Linkov <alexey.linkov@gmail.com>
2017-04-15 20:27:55 +0200

    FAL-20 Bird New / Edit profile; FAL-19	 Bird Profile selector (list view);

--- falconrypro/app/controllers/api/v1/birds_controller.rb
+++ falconrypro/app/controllers/api/v1/birds_controller.rb
@@ -2,4 +2,52 @@ class Api::V1::BirdsController < Api::ApiController
   def index
     render json: Bird.where(:user => current_user)
   end
+
+  def create
+    bird = current_user.birds.create(bird_params)
+    render :json => bird
+  end
+
+  def update
+    bird = Bird.find(params[:id])
+
+    #   Rails.logger.info "BLA"
+    # # Rails.logger.info params[:bird]
+    # Rails.logger.info params[:bird].name
+    params[:bird][:birdimage] = convert_to_upload(params[:bird][:birdimage])
+    #
+    bird.update(bird_params)
+    render :json => bird
+  end
+
+  protected
+
+def convert_to_upload(image)
+
+  temp_img_file = Tempfile.new("data_uri-upload")
+  temp_img_file.binmode
+  temp_img_file << Base64.decode64(image)
+  temp_img_file.rewind
+
+  ActionDispatch::Http::UploadedFile.new({
+                                             filename: "pic.png",
+                                             type: "image/png",
+                                             tempfile: temp_img_file
+                                         })
+end
+
+def split_base64(uri_str)
+  if uri_str.match(%r{^data:(.*?);(.*?),(.*)$})
+    uri = Hash.new
+    uri[:type] = $1 # "image/gif"
+    uri[:encoder] = $2 # "base64"
+    uri[:data] = $3 # data string
+    uri[:extension] = $1.split('/')[1] # "gif"
+    return uri
+  end
+end
+
+  def bird_params
+    params.require(:bird).permit!
+  end
 end
--- falconrypro/app/serializers/bird_serializer.rb
+++ falconrypro/app/serializers/bird_serializer.rb
@@ -1,9 +1,16 @@
 class BirdSerializer < ActiveModel::Serializer
-  attributes :id, :name, :thumb, :pic
+  attributes :id, :name, :thumb, :pic, :fat_weight, :hunting_weight, :code, :birthday, :type_name, :sex
   belongs_to :bird_type
 
+  def id
+    object.id.to_s
+  end
   def thumb
     object.birdimage.url(:thumb)
+  end
+
+  def type_name
+    object.bird_type.name
   end
 
   def pic
