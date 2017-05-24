c16103311914d01342afd5438253fe6f22b6bf42
Alex Linkov <alexey.linkov@gmail.com>
2017-05-24 17:59:44 +0200

    FAL-52; multi-adds on diary detail view;

--- falconrypro/app/admin/diary_item.rb
+++ falconrypro/app/admin/diary_item.rb
@@ -3,4 +3,43 @@ ActiveAdmin.register DiaryItem do
   before_filter :skip_sidebar!, :only => :index
   menu parent: 'User generated'
 
+  show do
+
+
+  panel "General" do
+
+    attributes_table do
+      row :note
+      row :bird
+      row :season
+    end
+
+
+  end
+
+
+  panel "Foods" do
+
+        table_for diary_item.diary_foods do
+          column :food
+          column :time
+          column :eaten
+
+        end
+
+  end
+
+
+  panel "Weights" do
+
+            table_for diary_item.diary_weights do
+              column :time
+              column :weight
+
+            end
+  end
+
+
+end
+
 end
