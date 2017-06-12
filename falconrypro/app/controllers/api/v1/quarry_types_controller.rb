7bce97197bac4774a2e8f88612d5bfe02b2a745d
Alex Linkov <alexey.linkov@gmail.com>
2017-06-12 10:31:37 +0200

    FAL-110; quarry caught custom input option;

--- falconrypro/app/controllers/api/v1/quarry_types_controller.rb
+++ falconrypro/app/controllers/api/v1/quarry_types_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::QuarryTypesController < Api::ApiController
   def index
     render json: QuarryType.all
   end
+
+
+  def create
+    custom_group = QuarryGroup.where(:name => "custom").first
+    quarry = QuarryType.create(quarry_params)
+    quarry.quarry_group_id = custom_group.id
+    quarry.save!
+    render :json => quarry
+  end
+
+  def quarry_params
+    params.require(:quarry_type).permit!
+  end
+
+
 end
